f10217a9b4636bd66aeea3a79e85ebc6892a466e
= <=>
2014-04-29 18:08:23 -0700

    Complete ControllerBase and Session implementation; begin Params.

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -6,9 +6,12 @@ class Params
   # 2. post body
   # 3. route params
   def initialize(req, route_params = {})
+    @params ||= {}
+    parse_www_encoded_form(req.query_string)
   end
 
   def [](key)
+    @params[key]
   end
 
   def permit(*keys)
@@ -32,10 +35,37 @@ class Params
   # should return
   # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
   def parse_www_encoded_form(www_encoded_form)
+    return if www_encoded_form.nil?
+    pairs = URI.decode_www_form(www_encoded_form)
+    p pairs
+    pairs.each do |pair|
+      key = pair.first
+      val = pair.last
+
+      split_keys = parse_key(key)
+      build_params(split_keys, val)
+    end
+  end
+
+  def build_params(split_keys, val)
+    return if split_keys.nil? || split_keys.empty?
+    key = split_keys
+    res = {}
+    if split_keys.length == 1
+      @params[key] = val
+    else
+      @params[key] = build_params(split_keys[1..-1], val)
+    end
+    # p @params
   end
 
   # this should return an array
   # user[address][street] should return ['user', 'address', 'street']
   def parse_key(key)
+    if key.include?('[') || key.include?(']')
+      key.delete(']').split('[')
+    else
+      key
+    end
   end
 end
